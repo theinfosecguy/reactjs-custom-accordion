0c86c42f9ccca49761602beb5d930909175f06c2
theinfosecguy <keshavaarav22@gmail.com>
2022-04-19 16:34:01 +0530

    Formatting Fix

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,98 +2,104 @@ import React, { useState } from 'react'
 import { AiOutlinePlusSquare, AiOutlineMinusSquare } from 'react-icons/ai'
 import '../css/Accordion.css'
 import {
-  getButtonStyles,
-  widthStyles,
-  backgroundStyles
+    getButtonStyles,
+    widthStyles,
+    backgroundStyles
 } from '../helpers/getStyles'
 
 export const Accordion = ({
-  data,
-  PlusIcon,
-  MinusIcon,
-  headerBackground,
-  footerBackground,
-  plusIconSize,
-  minusIconSize,
-  plusIconColor,
-  minusIconColor,
-  accordionWidth,
-  roundedBorders = false,
-  defaultOpen = false,
-  defaultOpenIndex = 0,
-  defaultAllOpen = false,
-  rootClass = '',
-  cardClass = '',
-  headerClass = '',
-  footerClass = ''
+    data,
+    PlusIcon,
+    MinusIcon,
+    headerBackground,
+    footerBackground,
+    plusIconSize,
+    minusIconSize,
+    plusIconColor,
+    minusIconColor,
+    accordionWidth,
+    roundedBorders = false,
+    defaultOpen = false,
+    defaultOpenIndex = 0,
+    defaultAllOpen = false,
+    rootClass = '',
+    cardClass = '',
+    headerClass = '',
+    footerClass = ''
 }) => {
-  const [open, setOpen] = useState(defaultOpen)
-  const [openIndex, setOpenIndex] = useState(defaultOpenIndex)
+    const [open, setOpen] = useState(defaultOpen)
+    const [openIndex, setOpenIndex] = useState(defaultOpenIndex)
 
-  const plusIconStyles = getButtonStyles(plusIconColor, plusIconSize)
-  const minusIconStyles = getButtonStyles(minusIconColor, minusIconSize)
-  const cardStyles = widthStyles(accordionWidth)
-  const headerStyles = backgroundStyles(headerBackground)
-  const footerStyles = backgroundStyles(footerBackground)
+    const plusIconStyles = getButtonStyles(plusIconColor, plusIconSize)
+    const minusIconStyles = getButtonStyles(minusIconColor, minusIconSize)
+    const cardStyles = widthStyles(accordionWidth)
+    const headerStyles = backgroundStyles(headerBackground)
+    const footerStyles = backgroundStyles(footerBackground)
 
-  const handleOnClick = (index) => {
-    setOpen(!open)
-    setOpenIndex(index)
-    openIndex !== index && setOpen(true)
-  }
+    const handleOnClick = (index) => {
+        setOpen(!open)
+        setOpenIndex(index)
+        openIndex !== index && setOpen(true)
+    }
 
-  return (
-    <section className={`accordion-root ${rootClass}`}>
-      {data.map((item, index) => {
-        return (
-          <div
-            className={`accordion-card ${cardClass}`}
-            key={index}
-            style={cardStyles}
-          >
-            <div
-              style={headerStyles}
-              className={`accordion-head flex bg-gray-900 text-white ${
-                roundedBorders
-                  ? open && openIndex === index
-                    ? 'rounded-t'
-                    : 'rounded'
-                  : ''
-              } ${headerClass}`}
-              onClick={() => handleOnClick(index)}
-            >
-              <span>{item?.title}</span>
-              {(open && openIndex === index) || defaultAllOpen ? (
-                MinusIcon ? (
-                  <MinusIcon style={minusIconStyles} className='minus-icon' />
-                ) : (
-                  <AiOutlineMinusSquare
-                    className='minus-icon'
-                    style={minusIconStyles}
-                  />
+    return (
+        <section className={`accordion-root ${rootClass}`}>
+            {data.map((item, index) => {
+                return (
+                    <div
+                        className={`accordion-card ${cardClass}`}
+                        key={index}
+                        style={cardStyles}
+                    >
+                        <div
+                            style={headerStyles}
+                            className={`accordion-head flex bg-gray-900 text-white ${
+                                roundedBorders
+                                    ? open && openIndex === index
+                                        ? 'rounded-t'
+                                        : 'rounded'
+                                    : ''
+                            } ${headerClass}`}
+                            onClick={() => handleOnClick(index)}
+                        >
+                            <span>{item?.title}</span>
+                            {(open && openIndex === index) || defaultAllOpen ? (
+                                MinusIcon ? (
+                                    <MinusIcon
+                                        style={minusIconStyles}
+                                        className='minus-icon'
+                                    />
+                                ) : (
+                                    <AiOutlineMinusSquare
+                                        className='minus-icon'
+                                        style={minusIconStyles}
+                                    />
+                                )
+                            ) : PlusIcon ? (
+                                <PlusIcon
+                                    style={plusIconStyles}
+                                    className='plus-icon'
+                                />
+                            ) : (
+                                <AiOutlinePlusSquare
+                                    className='plus-icon'
+                                    style={plusIconStyles}
+                                />
+                            )}
+                        </div>
+                        {(open && openIndex === index) || defaultAllOpen ? (
+                            <div
+                                className={`accordion-body ${
+                                    roundedBorders ? 'rounded-b' : ''
+                                } ${footerClass}`}
+                                style={footerStyles}
+                            >
+                                <span>{item?.description}</span>
+                            </div>
+                        ) : null}
+                    </div>
                 )
-              ) : PlusIcon ? (
-                <PlusIcon style={plusIconStyles} className='plus-icon' />
-              ) : (
-                <AiOutlinePlusSquare
-                  className='plus-icon'
-                  style={plusIconStyles}
-                />
-              )}
-            </div>
-            {(open && openIndex === index) || defaultAllOpen ? (
-              <div
-                className={`accordion-body ${
-                  roundedBorders ? 'rounded-b' : ''
-                } ${footerClass}`}
-                style={footerStyles}
-              >
-                <span>{item?.description}</span>
-              </div>
-            ) : null}
-          </div>
-        )
-      })}
-    </section>
-  )
+            })}
+        </section>
+    )
 }
--- src/helpers/getStyles.js
+++ src/helpers/getStyles.js
@@ -1,26 +1,26 @@
 const getButtonStyles = (
-  color,
-  size,
-  defaultColor = '#fff',
-  defaultSize = '1.3rem'
+    color,
+    size,
+    defaultColor = '#fff',
+    defaultSize = '1.3rem'
 ) => {
-  return {
-    fontSize: size ? size : defaultSize,
-    color: color ? color : defaultColor
-  }
+    return {
+        fontSize: size ? size : defaultSize,
+        color: color ? color : defaultColor
+    }
 }
 
 const widthStyles = (width, defaultWidth = '80%') => {
-  return {
-    width: width ? width : defaultWidth
-  }
+    return {
+        width: width ? width : defaultWidth
+    }
 }
 
 const backgroundStyles = (background, defaultColor) => {
-  return {
-    background: background ? background : defaultColor,
-    border: background === defaultColor ? '2px solid #333' : 'none'
-  }
+    return {
+        background: background ? background : defaultColor,
+        border: background === defaultColor ? '2px solid #333' : 'none'
+    }
 }
 
 export { getButtonStyles, widthStyles, backgroundStyles }
