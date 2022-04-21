a64e41903fd9a2136a38a9c758dcb1d2f77a84d2
theinfosecguy <keshavaarav22@gmail.com>
2022-04-21 20:57:57 +0530

    Updates

---
+++ precache-manifest.c8aab748b42fbb2b483b77f3640c7f6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34932d7db2ce7de2f153e30d41458283",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01e61de4a71c54cc339e",
+    "url": "./static/css/main.4d3bf653.chunk.css"
+  },
+  {
+    "revision": "6b7cc3cc644f4e498cfc",
+    "url": "./static/js/2.b87cbb8a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b87cbb8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e61de4a71c54cc339e",
+    "url": "./static/js/main.fe57cf93.chunk.js"
+  },
+  {
+    "revision": "0b43395ca9f6066a9786",
+    "url": "./static/js/runtime-main.3d25ec12.js"
+  }
+]);
